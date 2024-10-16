bfac900edecabee691308f6144734ce7412af8e9
Hoodrabbit <micyle1022@naver.com>
2024-10-17 01:06:36 +0900

    롱노트 판정 범위 약간 수정

--- Assets/Scripts/Now_Scripts/Note/LongNoteScript.cs
+++ Assets/Scripts/Now_Scripts/Note/LongNoteScript.cs
@@ -83,7 +83,7 @@ public class LongNoteScript : MonoBehaviour
         else
         {
 
-            if (Vector3.Distance(transform.position, Tail.transform.position) <= 0.2f || transform.position.x >= Tail.transform.position.x)
+            if (Vector3.Distance(transform.position, Tail.transform.position) <= 1.3f || transform.position.x >= Tail.transform.position.x)
                 {
                     Debug.Log("²¨Áü");
                 Delete = true;
